07ccdbc55f702834e9f8e6ee928b3c91c430dd21
mahanrah <156537885+mahanrah@users.noreply.github.com>
2024-04-22 11:09:01 +0100

    Update server.R to include licence

--- server.R
+++ server.R
@@ -1,3 +1,9 @@
+#Copyright (c) 2024, Matthew Hanrahan
+#All rights reserved.
+
+#This source code is licensed under the MIT license found in the
+#LICENSE file in the root directory of this source tree. 
+
 # Server
 
 #Shiny
